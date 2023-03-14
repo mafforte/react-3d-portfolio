a8533927e538a8aef237a7de499cfde80d334cc1
Raphael Mafforte <mafforte@gmail.com>
2023-03-14 19:03:20 -0300

    initial setup and configurations

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero";
+import About from "./components/About"
+import Works from "./components/Works"
+import Contact from "./components/Contact"
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      <About />
+      <Works />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
